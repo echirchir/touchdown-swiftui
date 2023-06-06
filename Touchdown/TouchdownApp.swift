0b30eb7f3e961f470733c82957e5eaf3ee82b6e8
Elisha Chirchir <elisha.java@gmail.com>
2023-06-06 19:43:58 +0300

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Elisha Chirchir on 06/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
